2a7160096c0f2a2f5b23039b0bcf0d07c96f62de
豊岡祐人 <toyooka.yuto@baroque-w.jp>
2020-11-12 16:42:10 +0900

    #API通信

--- frontend/todos/urls.py
+++ frontend/todos/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('show', views.show, name='show'),
+    path('get', views.get, name='get'),
 ]
--- frontend/todos/views.py
+++ frontend/todos/views.py
@@ -1,5 +1,20 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+import requests
 
+    
 def index(request):
     return HttpResponse("こんちには！")
+
+def show(request):
+    print("show")
+    return render(request, 'todos/hello.html')
+
+def get(request):
+    print("get")
+    url = 'http://127.0.0.1:8080/api/'
+    r = requests.get(url)
+    response = r.json()
+    print("response", response)
+    return render(request, 'todos/todos.html',  {'todos': response })
